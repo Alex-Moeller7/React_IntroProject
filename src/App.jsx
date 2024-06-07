fc9ec635c58afad1dfa36a4ff92b28eac31b450a
Alex Moeller <alex_moeller771@icloud.com>
2024-06-07 15:49:35 -0500

    Initial files and beginning of starter app

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import reactIMG from "./assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const descriptor = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactIMG} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>{descriptor} React concepts you will need for almost any app you are going to build!</p>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
